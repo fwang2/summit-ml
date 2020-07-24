981db64864a4e15632d03d9b823c483827a15156
Feiyi Wang <fwang2@gmail.com>
2020-07-23 20:36:08 -0400

    add missing pys

---
+++ preface.py
@@ -0,0 +1,14 @@
+%matplotlib inline
+import numpy as np
+import pandas as pd
+import scipy as sp
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+
+# setup matplotlib
+mpl.rcParams['font.family']='monospace'
+mpl.rcParams['font.monospace']='Fira Code'
+mpl.rcParams['font.size']=14.0
+mpl.rcParams['figure.figsize']=[8,6]
+
---
+++ users.py
@@ -0,0 +1,19 @@
+#
+stf008_users = {
+        'adisney1': 13022,
+        'atchley': 58031,
+        'bing': 4096,
+        'cbrumgard': 13401,
+        'cjzimmer': 11647,
+        'fwang2': 7396,
+        'griffith': 58438,
+        'hs2': 8951,
+        'jsimmons': 2004,
+        'oral': 17303,
+        'shinw': 13033,
+        'vazhkuda': 27048,
+        'vyw': 26737,
+        'xmr': 2363
+        }
+
+
